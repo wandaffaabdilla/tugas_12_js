1a1c9c89c4fb8d78568e281fdd90f9bd69b16632
wan daffa abdilla <wandaffaadilla@gmail.com>
2022-02-10 23:38:09 +0800

    Tugas 12 JS
    Tugas ini kami kumpulkan untuk niomic

---
+++ tugas12.js
@@ -0,0 +1,15 @@
+function tugas12(){
+    var buah = ['Pisang','Jeruk','Apel','Mangga'];
+    console.log(buah);
+    buah.pop();
+    
+    return buah;
+}
+    function tugas12a(){
+        var buah2 = ['Pisang','Jeruk','Apel','Mangga'];
+        buah2.pop();
+        buah2.shift();
+        return buah2;
+    }
+console.log(tugas12());
+console.log(tugas12a());
